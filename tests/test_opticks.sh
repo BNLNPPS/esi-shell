07eea786cdd88cb2b3f580930ec7bab89615c216
Dmitri Smirnov <dmixsmi@gmail.com>
2024-08-01 17:50:19 -0400

    test: run ctest for installed opticks packages

--- tests/test_opticks.sh
+++ tests/test_opticks.sh
@@ -1,6 +1,18 @@
-#!/usr/bin/env -S bash -l
+#!/usr/bin/env bash
+
+set -e
 
 HOME=${HOME/\/root/$ESI_DIR}
 
 opticks-full-prepare
-opticks-t
+ctest --test-dir $OPTICKS_PREFIX/build/okconf
+ctest --test-dir $OPTICKS_PREFIX/build/sysrap -E STTFTest
+ctest --test-dir $OPTICKS_PREFIX/build/ana
+ctest --test-dir $OPTICKS_PREFIX/build/analytic
+ctest --test-dir $OPTICKS_PREFIX/build/bin
+ctest --test-dir $OPTICKS_PREFIX/build/CSG -E "CSGNodeTest|CSGPrimSpecTest|CSGPrimTest|CSGFoundryTest|CSGFoundry_getCenterExtent_Test|CSGFoundry_findSolidIdx_Test|CSGNameTest|CSGTargetTest|CSGTargetGlobalTest|CSGFoundry_MakeCenterExtentGensteps_Test|CSGFoundry_getFrame_Test|CSGFoundry_getFrameE_Test|CSGFoundry_getMeshName_Test|CSGFoundryLoadTest|CSGQueryTest|CSGSimtraceTest|CSGSimtraceRerunTest|CSGSimtraceSampleTest|CSGCopyTest"
+ctest --test-dir $OPTICKS_PREFIX/build/qudarap -E "QSimTest|QOpticalTest|QSim_Lifecycle_Test|QSimWithEventTest"
+ctest --test-dir $OPTICKS_PREFIX/build/CSGOptiX -E CSGOptiXRenderTest
+ctest --test-dir $OPTICKS_PREFIX/build/gdxml
+ctest --test-dir $OPTICKS_PREFIX/build/u4 -E "U4GDMLReadTest|U4RandomTest|U4TraverseTest"
+ctest --test-dir $OPTICKS_PREFIX/build/g4cx -E G4CXRenderTest
